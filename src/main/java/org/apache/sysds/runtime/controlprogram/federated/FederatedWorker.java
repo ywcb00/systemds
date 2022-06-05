b6addd91cde4ac38923f675c0dd933964b5c93ad
ywcb00 <ywcb00@ywcb.org>
2022-06-05 16:32:44 +0200

    exp(**): start netty's worker event executor group with the limit of one thread (single-threaded)

--- src/main/java/org/apache/sysds/runtime/controlprogram/federated/FederatedWorker.java
+++ src/main/java/org/apache/sysds/runtime/controlprogram/federated/FederatedWorker.java
@@ -91,12 +91,12 @@ public class FederatedWorker {
 
 	private void run() {
 		log.info("Setting up Federated Worker on port " + _port);
-		int par_conn = ConfigurationManager.getDMLConfig().getIntValue(DMLConfig.FEDERATED_PAR_CONN);
-		final int EVENT_LOOP_THREADS = (par_conn > 0) ? par_conn : InfrastructureAnalyzer.getLocalParallelism();
+		// int par_conn = ConfigurationManager.getDMLConfig().getIntValue(DMLConfig.FEDERATED_PAR_CONN);
+		// final int EVENT_LOOP_THREADS = (par_conn > 0) ? par_conn : InfrastructureAnalyzer.getLocalParallelism();
 		NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
 		ThreadPoolExecutor workerTPE = new ThreadPoolExecutor(1, Integer.MAX_VALUE, 10, TimeUnit.SECONDS,
 			new SynchronousQueue<Runnable>(true));
-		NioEventLoopGroup workerGroup = new NioEventLoopGroup(EVENT_LOOP_THREADS, workerTPE);
+		NioEventLoopGroup workerGroup = new NioEventLoopGroup(1, workerTPE);
 
 		final boolean ssl = ConfigurationManager.isFederatedSSL();
 		try {
