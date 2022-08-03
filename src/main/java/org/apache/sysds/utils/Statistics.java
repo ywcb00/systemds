18d892acb38b76052513913d2c6aa82ec6f31f9f
ywcb00 <ywcb00@ywcb.org>
2022-08-03 10:14:27 +0200

    feat(MemConsumption): print the initial mem consumption at every call to createResponse()

--- src/main/java/org/apache/sysds/utils/Statistics.java
+++ src/main/java/org/apache/sysds/utils/Statistics.java
@@ -696,8 +696,8 @@ public class Statistics
 			if(mct == null || mct.isStopped()) {
 				mct = new MemConsumption();
 				new Thread(mct).start();
-				printInitialMemConsumption();
 			}
+			printInitialMemConsumption();
 		}
 
 		public void run() {
